ecd738983322cf330ad1dfd220c5e69071f01a5d
Barosl LEE <vcs@barosl.com>
2014-03-22 09:08:14 +0900

    chat-server.py: Use JSON instead of line-based protocol

--- chat-server.py
+++ chat-server.py
@@ -36,55 +36,58 @@ def get_new_nick():
 
 @utils.display_errors
 def proc(sock, path):
+    send = lambda x, d: x.send(json.dumps(d))
+
     with user_ctx(sock) as user:
         user.nick = get_new_nick()
-        yield from sock.send('nick: '+user.nick)
+        yield from send(sock, {'nick': user.nick})
 
-        yield from sock.send('msgs: '+json.dumps(msgs[-1000:]))
+        yield from send(sock, {'msgs': msgs[-1000:]})
 
-        users_s = json.dumps([x.nick for x in socks.values()])
+        data_s = json.dumps({'users': [x.nick for x in socks.values()]})
         for x in socks:
-            yield from x.send('users: '+users_s)
+            yield from x.send(data_s)
 
         while True:
             msg = yield from sock.recv()
             if not msg: break
 
-            cmd, msg = msg.split(': ', 1)
-            msg = msg.strip()
+            data = utils.AttrDict(json.loads(msg))
 
-            if cmd == 'nick':
-                nick = msg
+            if 'nick' in data:
+                nick = data.nick.strip()
 
                 if not validate_nick(nick):
-                    yield from sock.send('err: Invalid nickname')
+                    yield from send(sock, {'err': 'Invalid nickname'})
                     continue
 
                 if find_nick(nick):
-                    yield from sock.send('err: Nickname already in use')
+                    yield from send(sock, {'err': 'Nickname already in use'})
                     continue
 
                 user.nick = nick
-                yield from sock.send('nick: '+nick)
+                yield from send(sock, {'nick': nick})
 
-            elif cmd == 'msg':
+            elif 'msg' in data:
+                msg = data.msg.strip()
+
                 if not msg:
-                    yield from sock.send('err: Empty message')
+                    yield from send(sock, {'err': 'Empty message'})
                     continue
 
                 if not user.nick:
-                    yield from sock.send('err: Nickname not set')
+                    yield from send(sock, {'err': 'Nickname not set'})
                     continue
 
                 msg = '{nick}: {msg}'.format(nick=user.nick, msg=msg)
                 msgs.append(msg)
 
                 for x in socks:
-                    yield from x.send('msg: '+msg)
+                    yield from send(x, {'msg': msg})
 
-    users_s = json.dumps([x.nick for x in socks.values()])
+    data_s = json.dumps({'users': [x.nick for x in socks.values()]})
     for x in socks:
-        yield from x.send('users: '+users_s)
+        yield from x.send(data_s)
 
 def main():
     global msgs
